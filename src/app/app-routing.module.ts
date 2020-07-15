5874f67018891a121c10c96053be0f8b49881d4f
audihurrr <milesvendetti@gmail.com>
2020-07-15 14:16:15 -0400

    Changes layout colour, adds routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SectionSalesComponent } from  './sections/section-sales/section-sales.component';
+import { SectionHealthComponent } from  './sections/section-health/section-health.component';
+import { SectionOrdersComponent } from  './sections/section-orders/section-orders.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'sales', component: SectionSalesComponent },
+    { path: 'health', component: SectionHealthComponent },
+    { path: 'orders', component: SectionOrdersComponent },
+    { path: '', redirectTo: '/sales', pathMatch: 'full' }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
