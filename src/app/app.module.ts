8ad0cd339ff1a69d135da1d240077a71a80bcba0
audihurrr <milesvendetti@gmail.com>
2020-07-15 23:42:34 -0400

    Adds pie, bar, and line chart into bootstrap card-decks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,11 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { SectionSalesComponent } from './sections/section-sales/section-sales.component';
 import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
 import { SectionHealthComponent } from './sections/section-health/section-health.component';
-
-import { ChartsModule } from 'chart.js';
-import { BarChartComponent } from './bar-chart/bar-chart.component';
-import { LineChartComponent } from './line-chart/line-chart.component';
+ 
+import { ChartsModule } from 'ng2-charts';
+import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
+import { LineChartComponent } from './charts//line-chart/line-chart.component';
+import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
@@ -23,12 +24,14 @@ import { LineChartComponent } from './line-chart/line-chart.component';
     SectionOrdersComponent,
     SectionHealthComponent,
     BarChartComponent,
-    LineChartComponent
+    LineChartComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ChartsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
