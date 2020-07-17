bed7c0e610c57639fd50f8182a82ac91b17a7c23
audihurrr <milesvendetti@gmail.com>
2020-07-17 00:40:58 -0400

    Adds order and customer interfaces, order history table, with small set of test data

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Order } from '../../shared/order';
 @Component({
   selector: 'app-section-orders',
   templateUrl: './section-orders.component.html',
@@ -8,6 +8,48 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
+
+  orders : Order[] = [
+    { 
+      id: 1, 
+      customer: { 
+        id: 1, 
+        name: 'Yings Bakery', 
+        email: 'ying@gmail.com', 
+        state: 'CO' 
+      }, 
+      total: 130, 
+      fulfilled: new Date(2020, 7, 16), 
+      placed: new Date(2020,6,29), 
+      //status:'Completed'  
+    },
+    { 
+      id: 2, 
+      customer: { 
+        id: 2, 
+        name: 'Mings Cafe', 
+        email: 'ying@gmail.com', 
+        state: 'CO' 
+      }, 
+      total: 100, 
+      fulfilled: new Date(2020, 7, 13), 
+      placed: new Date(2020,6,25), 
+      //status:'Completed'  
+    },
+    { 
+      id: 3, 
+      customer: { 
+        id: 3, 
+        name: 'Yangs Bakery', 
+        email: 'yang@gmail.com', 
+        state: 'CO' 
+      }, 
+      total: 87.5, 
+      fulfilled: new Date(2020, 5, 10), 
+      placed: new Date(2020, 4, 3), 
+      //status:'Completed'  
+    },
+  ];
 
   ngOnInit(): void {
   }
